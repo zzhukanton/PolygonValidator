845e7c39efee8364ccc9d019cb7c961129041cb2
zzhukanton <zzhukanton@gmail.com>
2021-05-20 21:55:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace PolygonValidator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+			this.openFile = new System.Windows.Forms.OpenFileDialog();
+			this.playground = new System.Windows.Forms.PictureBox();
+			this.findButton = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.playground)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1333, 30);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// файлToolStripMenuItem
+			// 
+			this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+			this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+			this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+			this.файлToolStripMenuItem.Text = "Файл";
+			// 
+			// открытьToolStripMenuItem
+			// 
+			this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+			this.открытьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+			this.открытьToolStripMenuItem.Text = "Открыть";
+			this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+			// 
+			// сохранитьToolStripMenuItem
+			// 
+			this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+			this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+			this.сохранитьToolStripMenuItem.Text = "Сохранить";
+			// 
+			// gMapControl1
+			// 
+			this.gMapControl1.Bearing = 0F;
+			this.gMapControl1.CanDragMap = true;
+			this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+			this.gMapControl1.GrayScaleMode = false;
+			this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+			this.gMapControl1.LevelsKeepInMemmory = 5;
+			this.gMapControl1.Location = new System.Drawing.Point(903, 191);
+			this.gMapControl1.MarkersEnabled = true;
+			this.gMapControl1.MaxZoom = 2;
+			this.gMapControl1.MinZoom = 2;
+			this.gMapControl1.MouseWheelZoomEnabled = true;
+			this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+			this.gMapControl1.Name = "gMapControl1";
+			this.gMapControl1.NegativeMode = false;
+			this.gMapControl1.PolygonsEnabled = true;
+			this.gMapControl1.RetryLoadTile = 0;
+			this.gMapControl1.RoutesEnabled = true;
+			this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+			this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+			this.gMapControl1.ShowTileGridLines = false;
+			this.gMapControl1.Size = new System.Drawing.Size(418, 427);
+			this.gMapControl1.TabIndex = 1;
+			this.gMapControl1.Zoom = 0D;
+			// 
+			// openFile
+			// 
+			this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
+			// 
+			// playground
+			// 
+			this.playground.Location = new System.Drawing.Point(12, 31);
+			this.playground.Name = "playground";
+			this.playground.Size = new System.Drawing.Size(729, 587);
+			this.playground.TabIndex = 2;
+			this.playground.TabStop = false;
+			// 
+			// findButton
+			// 
+			this.findButton.Location = new System.Drawing.Point(773, 55);
+			this.findButton.Name = "findButton";
+			this.findButton.Size = new System.Drawing.Size(224, 94);
+			this.findButton.TabIndex = 3;
+			this.findButton.Text = "Найти точки пересечения";
+			this.findButton.UseVisualStyleBackColor = true;
+			this.findButton.Click += new System.EventHandler(this.findButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1333, 630);
+			this.Controls.Add(this.findButton);
+			this.Controls.Add(this.playground);
+			this.Controls.Add(this.gMapControl1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "Валидатор полигонов";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.playground)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+		private GMap.NET.WindowsForms.GMapControl gMapControl1;
+		private System.Windows.Forms.OpenFileDialog openFile;
+		private System.Windows.Forms.PictureBox playground;
+		private System.Windows.Forms.Button findButton;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PolygonValidator
+{
+	public partial class MainForm : Form
+	{
+		private Point[] polygonPoints; 
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// read file, get the list of points
+			openFile.ShowDialog();
+		}
+
+		private void openFile_FileOk(object sender, CancelEventArgs e)
+		{
+			//openFile.
+			string fileName = openFile.FileName;
+			string[] stringPoints = File.ReadAllText(fileName).Split(new[] { ',' });
+			this.polygonPoints = new Point[stringPoints.Length];
+
+			for (int i = 0; i < stringPoints.Length; i++)
+			{
+				string[] coordinates = stringPoints[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+				this.polygonPoints[i] = new Point
+				{
+					X = int.Parse(coordinates[0]),
+					Y = int.Parse(coordinates[1])
+				};
+			}
+
+			DrawPolygon(this.polygonPoints);
+		}
+
+		private void DrawPolygon(Point[] polygonPoints)
+		{
+			playground.Image = new Bitmap(playground.Width, playground.Height);
+			using (Graphics g = Graphics.FromImage(playground.Image))
+			{
+				g.Clear(Color.White);
+
+				Pen blackPen = new Pen(Color.Black, 1);
+				g.DrawLines(blackPen, polygonPoints);
+			}
+		}
+
+		private void findButton_Click(object sender, EventArgs e)
+		{
+			List<Point> intersections = new List<Point>();
+
+			for (int i = 0; i < this.polygonPoints.Length - 4; i++)
+			{
+				Point? intersection = CheckForIntersection(
+					new Line
+					{
+						Start = this.polygonPoints[i],
+						End = this.polygonPoints[i + 1]
+					},
+					new Line
+					{
+						Start = this.polygonPoints[i + 2],
+						End = this.polygonPoints[i + 3]
+					});
+
+				if (intersection != null)
+				{
+					intersections.Add(intersection.Value);
+				}
+			}
+
+			if (intersections.Count > 0)
+			{
+				HighlightIntersections(intersections);
+			}
+		}
+
+		private void HighlightIntersections(List<Point> intersections)
+		{
+			using (Graphics g = Graphics.FromImage(playground.Image))
+			{
+				Pen redPen = new Pen(Color.Red, 2);
+
+				intersections.ForEach(point =>
+				{
+					g.DrawEllipse(redPen, point.X, point.Y, 1, 1);
+				});
+			}
+		}
+
+		private Point? CheckForIntersection(Line first, Line third)
+		{
+			// сортируем точки
+			Point p1 = new Point { X = first.Start.X, Y = first.Start.Y };
+			Point p2 = new Point { X = first.End.X, Y = first.End.Y };
+			Point p3 = new Point { X = third.Start.X, Y = third.Start.Y };
+			Point p4 = new Point { X = third.End.X, Y = third.End.Y };
+
+			//проверим существование потенциального интервала для точки пересечения отрезков
+			if (p2.X < p1.X)
+			{
+				//ибо у отрезков нету взаимной абсциссы
+				return null; 
+			}
+
+			//найдём коэффициенты уравнений, содержащих отрезки
+			//f1(x) = A1*x + b1 = y
+			//f2(x) = A2*x + b2 = y
+			//если первый отрезок вертикальный
+			if (p1.X - p2.X == 0)
+			{
+				//найдём Xa, Ya - точки пересечения двух прямых
+				double resX = p1.X;
+				double tg = (p3.Y - p4.Y) / (p3.X - p4.X);
+				double shift = p3.Y - tg * p4.X;
+
+				double resultY = tg * resX + shift;
+
+				if (p3.X <= resX &&
+					p4.X >= resX && 
+					Math.Min(p1.Y, p2.Y) <= resultY && 
+					Math.Max(p1.Y, p2.Y) >= resultY)
+				{
+					return new Point { X = (int)Math.Round(resX), Y = (int)Math.Round(resultY) };
+				}
+
+				return null;
+			}
+
+			//если второй отрезок вертикальный
+			if (p3.X - p4.X == 0)
+			{
+				//найдём Xa, Ya - точки пересечения двух прямых
+				double resX = p3.X;
+				double tg = (p1.Y - p2.Y) / (p1.X - p2.X);
+				double shift = p1.Y - tg * p1.X;
+				double resultY = tg * resX + shift;
+
+				if (p1.X <= resX && 
+					p2.X >= resX && 
+					Math.Min(p3.Y, p4.Y) <= resultY && 
+					Math.Max(p3.Y, p4.Y) >= resultY)
+				{
+					return new Point { X = (int)Math.Round(resX), Y = (int)Math.Round(resultY) };
+				}
+
+				return null;
+			}
+
+			//оба отрезка невертикальные
+			double tg1 = (p1.Y - p2.Y) / (p1.X - p2.X);
+			double tg2 = (p3.Y - p4.Y) / (p3.X - p4.X);
+			double shift1 = p1.Y - tg1 * p1.X;
+			double shift2 = p3.Y - tg2 * p4.X;
+
+			if (tg1 == tg2)
+			{
+				//отрезки параллельны
+				return null; 
+			}
+
+			//Xa - абсцисса точки пересечения двух прямых
+			double resultX = (shift2 - shift1) / (tg1 - tg2);
+
+			if ((resultX < Math.Max(p1.X, third.Start.X)) || (resultX > Math.Min(first.End.X, third.End.X)))
+			{
+				//точка Xa находится вне пересечения проекций отрезков на ось X
+				return null;
+			}
+			else
+			{
+				double resultY = tg1 * resultX + shift1;
+				return new Point { X = (int)Math.Round(resultX), Y = (int)Math.Round(resultY) };
+			}
+		}
+	}
+}
